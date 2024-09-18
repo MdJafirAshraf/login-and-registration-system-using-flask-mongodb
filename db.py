f3ec30849f474310a329cce7f99f219986dd4bc4
Mohamed Jafir Ashraf <63150299+JafirDon@users.noreply.github.com>
2024-09-18 08:27:27 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,36 +7,36 @@ users = userdb.customers
 
 
 def insert_data():
-	if request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
-		password = request.form['pass']
+    if request.method == 'POST':
+	name = request.form['name']
+	email = request.form['email']
+	password = request.form['pass']
 
-		reg_user = {}
-		reg_user['name'] = name
-		reg_user['email'] = email
-		reg_user['password'] = password
+	reg_user = {}
+	reg_user['name'] = name
+	reg_user['email'] = email
+	reg_user['password'] = password
 
-		if users.find_one({"email":email}) == None:
-			users.insert_one(reg_user)
-			return True
-		else:
-			return False
+	if users.find_one({"email":email}) == None:
+	    users.insert_one(reg_user)
+	    return True
+	else:
+	     return False
 
 
 def check_user():
+    if request.method == 'POST':
+	email = request.form['email']
+	password = request.form['pass']
 
-	if request.method == 'POST':
-		email = request.form['email']
-		password = request.form['pass']
+	user = {
+		"email": email,
+		"password": password
+	}
 
-		user = {
-			"email": email,
-			"password": password
-		}
+	user_data = users.find_one(user)
+	if user_data == None:
+	    return False, ""
+	else:
+	    return True, user_data["name"]
 
-		user_data = users.find_one(user)
-		if user_data == None:
-			return False, ""
-		else:
-			return True, user_data["name"]
